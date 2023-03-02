05fee179b1f8296e6e32bf74523809a8e47bde3c
Gabriel Guilherme <gabrielguilherme1000@hotmail.com>
2023-03-02 00:05:35 -0300

    Removendo imports desnecessários

--- src/main/java/com/datafipe/datafipeweb/model/MesReferencia.java
+++ src/main/java/com/datafipe/datafipeweb/model/MesReferencia.java
@@ -1,8 +1,5 @@
 package com.datafipe.datafipeweb.model;
 
-import javax.persistence.Entity;
-
-
 public class MesReferencia {
     
 
--- src/main/java/com/datafipe/datafipeweb/model/ModeloVeiculo.java
+++ src/main/java/com/datafipe/datafipeweb/model/ModeloVeiculo.java
@@ -1,9 +1,5 @@
 package com.datafipe.datafipeweb.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
